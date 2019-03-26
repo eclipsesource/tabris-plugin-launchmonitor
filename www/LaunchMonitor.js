f6fb6e66f4de365b26db3e887eca935dcd2250ff
Christian Petrov <cpetrov@eclipsesource.com>
2019-03-26 12:48:53 +0100

    Introduce property "urlLaunchParameters"
    Useful if listening to the "urlLaunch" event early on is not possible.
    
    Opt for a JS-only implementation of the property to prevent unneeded
    native implementation overhead.
    
    Change-Id: I2a4ffcc9ce3e970ebf7fef3936ef2e72fdff9276

--- www/LaunchMonitor.js
+++ www/LaunchMonitor.js
@@ -1,4 +1,7 @@
-var EVENT_TYPES = ['urlLaunch'];
+var URL_LAUNCH_EVENT = 'urlLaunch';
+var EVENT_TYPES = [URL_LAUNCH_EVENT];
+
+var urlLaunchParameters = null;
 
 var LaunchMonitor = tabris.NativeObject.extend('com.eclipsesource.tabris.LaunchMonitor');
 
@@ -9,5 +12,24 @@ LaunchMonitor.prototype._listen = function(name, listening) {
     tabris.Widget.prototype._listen.call(this, name, listening);
   }
 };
+
+LaunchMonitor.prototype._trigger = function(name, event) {
+  if (name === URL_LAUNCH_EVENT) {
+    urlLaunchParameters = {queryParameters: event.queryParameters};
+  }
+  tabris.Widget.prototype._trigger.call(this, name, event);
+};
+
+tabris.NativeObject.defineProperties(LaunchMonitor.prototype, {
+  urlLaunchParameters: {
+    type: 'any',
+    set: function(name) {
+      console.warn(`Can not set read-only property "${name}"`)
+    },
+    get: function() {
+      return urlLaunchParameters;
+    }
+  }
+})
 
 module.exports = new LaunchMonitor();
