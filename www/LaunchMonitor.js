7b9142e3fcc15c748661335f15269b46363541e9
Christian Petrov <cpetrov@eclipsesource.com>
2019-03-27 11:24:07 +0100

    Fix urlLaunchParameters property
    Previously the property would not get updated if no "urlLaunch" event
    listener got registered.
    
    Change-Id: I74dd37082b9246393d0cfc371deb5eac734dbbab

--- www/LaunchMonitor.js
+++ www/LaunchMonitor.js
@@ -1,17 +1,8 @@
 var URL_LAUNCH_EVENT = 'urlLaunch';
-var EVENT_TYPES = [URL_LAUNCH_EVENT];
 
 var urlLaunchParameters = null;
 
 var LaunchMonitor = tabris.NativeObject.extend('com.eclipsesource.tabris.LaunchMonitor');
-
-LaunchMonitor.prototype._listen = function(name, listening) {
-  if (EVENT_TYPES.indexOf(name) > -1) {
-    this._nativeListen(name, listening);
-  } else {
-    tabris.Widget.prototype._listen.call(this, name, listening);
-  }
-};
 
 LaunchMonitor.prototype._trigger = function(name, event) {
   if (name === URL_LAUNCH_EVENT) {
@@ -32,4 +23,6 @@ tabris.NativeObject.defineProperties(LaunchMonitor.prototype, {
   }
 })
 
-module.exports = new LaunchMonitor();
+let launchMonitor = new LaunchMonitor();
+launchMonitor._nativeListen(URL_LAUNCH_EVENT, true);
+module.exports = launchMonitor;
