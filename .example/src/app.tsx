228f8da080b9ea53eb7067bdfb341c79bc288008
Christian Petrov <cpetrov@eclipsesource.com>
2020-04-08 13:54:41 +0200

    Update .example/src/app.tsx
    Co-Authored-By: Moritz Post <mpost@eclipsesource.com>

--- .example/src/app.tsx
+++ .example/src/app.tsx
@@ -12,6 +12,6 @@ contentView.append(
 );
 
 eslaunchmonitor.on({urlLaunch: ({queryParameters, url}) => {
-  contentView.find(TextView).first().text = `Url: <br/><b>${url}</b><br/><br/>Query parameters: <br/><b>${JSON.stringify(queryParameters)}</b>`
+  contentView.find(TextView).only().text = `Url: <br/><b>${url}</b><br/><br/>Query parameters: <br/><b>${JSON.stringify(queryParameters)}</b>`
   console.log('"urlLaunch" parameters: ' + JSON.stringify({queryParameters, url}));
 }});
