e3bd4e90f8a027489145107e031050a9c30586a6
Christian Petrov <cpetrov@eclipsesource.com>
2020-04-08 18:41:39 +0200

    Update iOS implementation to Tabris.js 3.4
    Change-Id: I212704af5a6dad6d1d7e702dcfbcde2b0825a0de

--- src/ios/ESLaunchMonitor.h
+++ src/ios/ESLaunchMonitor.h
@@ -1,6 +1,6 @@
-#import <Tabris/BasicWidget.h>
+#import <Tabris/Control.h>
 
-@interface ESLaunchMonitor : BasicWidget
+@interface ESLaunchMonitor : Control
 @property (assign) BOOL urlLaunchListener;
 + (void)didOpenByURL:(NSURL *)url;
 @end
