30207b4a84eb8466be4971e755d4047992856d65
Elshad Seyidmammadov <elshad@eclipsesource.com>
2020-10-29 13:58:21 +0400

    Fix compile errors
    The change:
    * Fixes the error 'Smart cast to 'String' is impossible, because 'launchUri.query' is a property that has open or custom getter' which is thrown during an app build.
    * Adds 'fragment-ktx' implementation in order to fix compile error.
    * Upgrades kotlin, Gradle, and 'tabris-android' versions.

--- src/android/com/eclipsesource/tabris/launchmonitor/LaunchMonitor.kt
+++ src/android/com/eclipsesource/tabris/launchmonitor/LaunchMonitor.kt
@@ -17,7 +17,7 @@ class LaunchMonitor(scope: ActivityScope) {
         notifyUrlLaunch(scope, scope.activity.intent)
       }
     }
-      scope.events.addActivityStateListener(object : Events.ActivityStateListener {
+    scope.events.addActivityStateListener(object : Events.ActivityStateListener {
       override fun activityStateChanged(state: ActivityState, intent: Intent?) {
         when (state) {
           ActivityState.NEW_INTENT -> notifyUrlLaunch(scope, intent)
@@ -36,17 +36,18 @@ class LaunchMonitor(scope: ActivityScope) {
     scope.objectRegistry.findRemoteObject(this)?.notify(EVENT_URL_LAUNCH, urlLaunchParameters)
   }
 
-  private fun parseQuery(url:String?):Map<String, String>? {
+  private fun parseQuery(url: String?): Map<String, String>? {
     if (url == null) return HashMap<String, String>()
     val launchUri = Uri.parse(url)
     if (launchUri.query == null) return HashMap<String, String>()
     val result = HashMap<String, String>()
-    val pairs = launchUri.query.split("&").dropLastWhile { it.isEmpty() }.toTypedArray()
-    pairs.forEach {
-      val idx = it.indexOf("=")
-      val key = URLDecoder.decode(it.substring(0, idx), "UTF-8")
-      val value = URLDecoder.decode(it.substring(idx + 1), "UTF-8")
-      result[key] = value
+    launchUri?.query?.split("&")?.dropLastWhile { it.isEmpty() }?.toTypedArray()?.let { pairs ->
+      pairs.forEach {
+        val idx = it.indexOf("=")
+        val key = URLDecoder.decode(it.substring(0, idx), "UTF-8")
+        val value = URLDecoder.decode(it.substring(idx + 1), "UTF-8")
+        result[key] = value
+      }
     }
     return result
   }
